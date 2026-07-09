68d5bd9951b85e5850659bfb497e81b7c85dd815
Jingham2510 <jsphingham@gmail.com>
2026-07-09 14:42:37 +0100

    geomap update and id pos change

--- src/sys_cntrl/system_control.rs
+++ src/sys_cntrl/system_control.rs
@@ -375,9 +375,8 @@ impl SystemController{
         const BOARD_SIZE : f32 = 0.80;
         //Board to sand distance
         const BOARD_THICKNESS : f32 = 0.0185;
-        const MARKER_COORDS : [[f32; 3]; 4] = [[0.0, 0.0, BOARD_THICKNESS], [0.0, BOARD_SIZE, BOARD_THICKNESS], [BOARD_SIZE, 0.0, BOARD_THICKNESS], [BOARD_SIZE, BOARD_SIZE, BOARD_THICKNESS]];
-        const MARKER_IDS : [i32;4] = [0, 1, 2, 3];
-
+        const MARKER_COORDS : [[f32; 3]; 4] = [[0.0, 0.0, BOARD_THICKNESS], [BOARD_SIZE, 0.0, BOARD_THICKNESS],  [BOARD_SIZE, BOARD_SIZE, BOARD_THICKNESS], [0.0, BOARD_SIZE, BOARD_THICKNESS]];
+        const MARKER_IDS : [i32;4] = [1, 3, 2, 0];
         //For each image calculate the inverse extrinsics 
         for (i, image) in img_filepaths.iter().enumerate(){
             println!(">----------CAM: {}-------------", i);
