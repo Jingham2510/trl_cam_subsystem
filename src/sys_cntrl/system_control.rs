543db7cbf963d9ec57ca42d998df4922cf98e448
Jingham2510 <jsphingham@gmail.com>
2026-07-21 13:27:07 +0100

    test: force to tcp transform

--- src/sys_cntrl/system_control.rs
+++ src/sys_cntrl/system_control.rs
@@ -193,7 +193,7 @@ impl SystemController{
     pub fn fire_and_transform(&mut self) -> Result<Vec<PointCloud>, anyhow::Error>{
 
 
-        self.curr_pos = FRONT_SPOKE_POS;
+        self.curr_pos = [417.23, 2375.24, 1075.56];
         self.curr_ori = FRONT_SPOKE_ORI;
 
         let mut pcl_vec = self.fire_all_cams()?;
