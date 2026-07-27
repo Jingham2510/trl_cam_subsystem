b6517326a84b1b1af1ba6c391f8f88d923ff180a
Jingham2510 <jsphingham@gmail.com>
2026-07-27 14:49:17 +0100

    update ghmap bounds

--- src/sys_cntrl/system_control.rs
+++ src/sys_cntrl/system_control.rs
@@ -190,8 +190,8 @@ impl SystemController{
         }
 
         let mut global_hmap = Heightmap::new(GLOBAL_HMAP_WIDTH, GLOBAL_HMAP_HEIGHT);
-        global_hmap.set_lower_coord_bounds([-0.2, -0.1]);
-        global_hmap.set_upper_coord_bounds([0.85, 1.0]);
+        global_hmap.set_lower_coord_bounds([-0.18, -0.08]);
+        global_hmap.set_upper_coord_bounds([0.9, 0.98]);
         global_hmap.set_all_cells(f32::NAN);
 
 
