8248a358ff7a6b6994a640dc0b1c7692daa78220
Jingham2510 <jsphingham@gmail.com>
2026-09-02 14:53:28 +0100

    bugtesting

--- src/main.rs
+++ src/main.rs
@@ -251,13 +251,15 @@ fn command_handler(mut config_manager : ConfigManager){
                             }
                         }
 
-
+/*
                         let mut i = 0;
 
                         for pcl in pcls{
                             pcl.save_to_file(&format!("out/pcl_{i}"));
 			                i += 1;
                         }
+
+                        */
 
                         println!("Points captured");
 
